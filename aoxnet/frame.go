528a697a020dab646408cb49cfdc4fb616b3a094
Sayan the researcher <sm6881164@gmail.com>
2026-09-09 15:32:19 +0530

    fix: normalize AOXNet payload limit before validation

--- aoxnet/frame.go
+++ aoxnet/frame.go
@@ -10,11 +10,11 @@ import (
 )
 
 const (
-    Magic        uint32 = 0x414F5831 // AOX1
-    Version      byte   = 1
-    HeaderSize          = 24
-    TrailerSize         = 4
-    DefaultMaxPayload   = 1 << 20
+    Magic              uint32 = 0x414F5831 // AOX1
+    Version            byte   = 1
+    HeaderSize                = 24
+    TrailerSize               = 4
+    DefaultMaxPayload         = 1 << 20
 )
 
 type Type byte
@@ -39,10 +39,9 @@ type Frame struct {
 }
 
 func (f Frame) Encode(w io.Writer, maxPayload uint32) error {
+    if maxPayload == 0 { maxPayload = DefaultMaxPayload }
     if f.Version == 0 { f.Version = Version }
     if len(f.Payload) > int(maxPayload) { return fmt.Errorf("payload too large: %d", len(f.Payload)) }
-    if maxPayload == 0 { maxPayload = DefaultMaxPayload }
-
     h := make([]byte, HeaderSize)
     binary.BigEndian.PutUint32(h[0:4], Magic)
     h[4] = f.Version
@@ -51,16 +50,11 @@ func (f Frame) Encode(w io.Writer, maxPayload uint32) error {
     binary.BigEndian.PutUint32(h[8:12], f.StreamID)
     binary.BigEndian.PutUint64(h[12:20], f.RequestID)
     binary.BigEndian.PutUint32(h[20:24], uint32(len(f.Payload)))
-
-    crc := crc32.NewIEEE()
-    _, _ = crc.Write(h)
-    _, _ = crc.Write(f.Payload)
+    crc := crc32.NewIEEE(); _, _ = crc.Write(h); _, _ = crc.Write(f.Payload)
     if _, err := w.Write(h); err != nil { return err }
     if _, err := w.Write(f.Payload); err != nil { return err }
-    var trailer [4]byte
-    binary.BigEndian.PutUint32(trailer[:], crc.Sum32())
-    _, err := w.Write(trailer[:])
-    return err
+    var trailer [4]byte; binary.BigEndian.PutUint32(trailer[:], crc.Sum32())
+    _, err := w.Write(trailer[:]); return err
 }
 
 func Decode(r *bufio.Reader, maxPayload uint32) (Frame, error) {
