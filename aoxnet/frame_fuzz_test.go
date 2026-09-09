15e991826ef539f47ee3846f04061704a91361cf
Sayan the researcher <sm6881164@gmail.com>
2026-09-09 15:50:23 +0530

    fix: make frame fuzz target compile against decoder API

--- aoxnet/frame_fuzz_test.go
+++ aoxnet/frame_fuzz_test.go
@@ -1,11 +1,15 @@
 package aoxnet
 
-import "testing"
+import (
+    "bufio"
+    "bytes"
+    "testing"
+)
 
 func FuzzDecodeNeverPanics(f *testing.F) {
     f.Add([]byte("AOX1"))
     f.Add(make([]byte, HeaderSize+TrailerSize))
     f.Fuzz(func(t *testing.T, data []byte) {
-        _, _ = DecodeBytes(data, DefaultMaxPayload)
+        _, _ = Decode(bufio.NewReader(bytes.NewReader(data)), DefaultMaxPayload)
     })
 }
