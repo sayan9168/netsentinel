ba3d11951586f3f9ae0da28127e77d0f86452081
Sayan the researcher <sm6881164@gmail.com>
2026-09-11 12:24:58 +0530

    fix: make protocol error codes wrap-compatible

--- aoxnet/errors.go
+++ aoxnet/errors.go
@@ -1,5 +1,7 @@
 package aoxnet
 
+import "fmt"
+
 // ErrorCode identifies a protocol-level failure.
 type ErrorCode uint16
 
@@ -14,6 +16,30 @@ const (
 	ErrRateLimited
 	ErrInternal
 )
+
+// Error implements error so protocol codes can participate in errors.Is/errors.As and %w wrapping.
+func (e ErrorCode) Error() string {
+	switch e {
+	case ErrInvalidFrame:
+		return "invalid frame"
+	case ErrUnsupportedVersion:
+		return "unsupported version"
+	case ErrProtocolState:
+		return "protocol state error"
+	case ErrPayloadTooLarge:
+		return "payload too large"
+	case ErrTimeout:
+		return "timeout"
+	case ErrUnauthorized:
+		return "unauthorized"
+	case ErrRateLimited:
+		return "rate limited"
+	case ErrInternal:
+		return "internal error"
+	default:
+		return fmt.Sprintf("unknown error code %d", uint16(e))
+	}
+}
 
 // ProtocolError is a structured AOXNet protocol error.
 type ProtocolError struct {
